a7ca148b6e7083169954ea0268f8deee1805fbf9
SahilMehdiyev <branchsahil@gmail.com>
2024-10-27 12:30:36 +0400

    update static method

--- page/models.py
+++ page/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+STATUS = [
+    ('draft', 'Taslak'),
+    ('publihsed', 'Yayinlandi'),
+    ('deleted', 'Silindi'),
+]
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    # slug
+    content = models.TextField()
+    cover_image = models.ImageField(upload_to='page')
+    status = models.CharField(
+        default='draft',
+        choices=STATUS,
+        max_length=10,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
